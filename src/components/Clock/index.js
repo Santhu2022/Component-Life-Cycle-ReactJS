f032eb6415f3c813ad2d3bed6c4f52ae3b1a7ee4
Santhu2022 <mogilipakasantosh@gmail.com>
2023-04-24 15:26:59 +0000

    Full Code

---
+++ src/components/Clock/index.js
@@ -0,0 +1,36 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('Constructor Called')
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, 1000)
+    console.log('componentDidMount Called')
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+    console.log('componentWillUnmount Called')
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    const {date} = this.state
+    console.log('render Called')
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
